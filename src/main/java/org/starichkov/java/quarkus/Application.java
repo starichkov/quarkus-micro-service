6cd2c98da5a096b83488cce155ee7d20435a6607
Vadim Starichkov <starichkovva@gmail.com>
2021-08-06 13:22:32 +0300

    Added main class for easy launch from IDE

---
+++ src/main/java/org/starichkov/java/quarkus/Application.java
@@ -0,0 +1,16 @@
+package org.starichkov.java.quarkus;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+/**
+ * @author Vadim Starichkov (starichkovva@gmail.com)
+ * @since 06.08.2021 13:16
+ */
+@QuarkusMain
+public final class Application {
+
+    public static void main(String... args) {
+        Quarkus.run(args);
+    }
+}
