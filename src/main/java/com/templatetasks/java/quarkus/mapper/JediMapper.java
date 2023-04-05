9deeae08dc384e59aaee9271dbffc0e28b853d27
Vadim Starichkov <starichkovva@gmail.com>
2023-04-05 20:30:04 +0300

    Moved Mapstruct componentModel from pom.xml to the mapper itself

--- src/main/java/com/templatetasks/java/quarkus/mapper/JediMapper.java
+++ src/main/java/com/templatetasks/java/quarkus/mapper/JediMapper.java
@@ -1,14 +1,15 @@
 package com.templatetasks.java.quarkus.mapper;
 
-import org.mapstruct.Mapper;
 import com.templatetasks.java.quarkus.domain.JediEntity;
 import com.templatetasks.java.quarkus.dto.Jedi;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
 /**
  * @author Vadim Starichkov (starichkovva@gmail.com)
  * @since 03.08.2021 14:27
  */
-@Mapper
+@Mapper(componentModel = MappingConstants.ComponentModel.CDI)
 public interface JediMapper {
 
     Jedi map(JediEntity jediEntity);
