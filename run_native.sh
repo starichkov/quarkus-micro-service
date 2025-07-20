207393a41736c10b1eb693150070a4e44f9d1949
Vadim Starichkov <starichkovva@gmail.com>
2025-07-20 19:29:43 +0300

    resolves #6 - native build with GraalVM

---
+++ run_native.sh
@@ -0,0 +1,5 @@
+export DB_USERNAME=quark
+export DB_PASSWORD=boson
+#export POSTGRES_PASSWORD=PoStGrEs17p2aLpIne321
+./target/quarkus-micro-service-1.0.0-runner
+#java -jar target/quarkus-micro-service-1.0.0-runner.jar
